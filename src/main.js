c88a67265c4f2aad51b17d762cc1892caf14b13b
Spyros Karidis <spyrology@gmail.com>
2015-11-08 14:02:20 -0500

    Add Redux store

--- src/main.js
+++ src/main.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute } from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
+import {createStore} from 'redux';
+import reducer from './reducers/reducer';
 import routes from './routes';
 
 let history = createBrowserHistory();
+
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    orgs = [
+			{name: 'Noodle', position: 'Editor', status: 'Open', Requirements: 'Portfolio', Price: '99'},
+			{name: 'Thinkful', position: 'Marketing Coordinator', status: '3 months', Requirements: 'None', Price: '199'}
+		];
+  }
+});
 
 ReactDOM.render((
   <Router history={history} routes={routes} />
